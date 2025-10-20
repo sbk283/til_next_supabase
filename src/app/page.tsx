0ec57ef073831d2fac796ea3fd7f84941d4e1432
sbkcoding <sbkcoding@gmail.com>
2025-10-20 10:11:08 +0900

    [docs] 초기설정 및 shadcn 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import ButtonTest from '@/components/ButtonTest';
-import TestComponent from '@/components/TestComponent';
 
 export default function Home() {
   return (
     <div>
-      {/* <TestComponent title='안녕하세요' description='SCSS TEST' /> */}
       <ButtonTest />
     </div>
   );
