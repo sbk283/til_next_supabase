e0c2a8c93e9aa8da684dfbbacdf37865d25a06ed
sbkcoding <sbkcoding@gmail.com>
2025-10-20 09:38:47 +0900

    [docs] reset 진행

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ButtonTest from '@/components/ButtonTest';
 import TestComponent from '@/components/TestComponent';
 
 export default function Home() {
   return (
     <div>
-      <TestComponent title='안녕하세요' description='SCSS TEST' />
+      {/* <TestComponent title='안녕하세요' description='SCSS TEST' /> */}
+      <ButtonTest />
     </div>
   );
 }
