47017bf611fbee85ac0585368abf3d149ffb282c
sbkcoding <sbkcoding@gmail.com>
2025-10-28 14:23:18 +0900

    [docs] 에러처리

--- src/app/hooks/mutations/useSignInWithPassword.ts
+++ src/app/hooks/mutations/useSignInWithPassword.ts
@@ -1,8 +1,18 @@
 import { signInWithPassword } from '@/apis/auth';
+import { UseMutationCallback } from '@/types/types';
 import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
-export function useSignInWithPassword() {
+export function useSignInWithPassword(callback?: UseMutationCallback) {
   return useMutation({
     mutationFn: signInWithPassword,
+    // 자동으로 error 전달받음
+    onError: error => {
+      console.error(error);
+      // Sonner 로 띄우기
+      toast.error(error.message, { position: 'top-center' });
+
+      if (callback?.onError) callback.onError(error);
+    },
   });
 }
