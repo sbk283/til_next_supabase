e8dd07dd122c5866e7d3962b588c8047fdd13ae5
sbkcoding <sbkcoding@gmail.com>
2025-11-11 10:06:35 +0900

    [docs] 포스트 글등록

---
+++ src/apis/post.ts
@@ -0,0 +1,7 @@
+import supabase from '@/lib/supabase/client';
+
+export async function createPost(content: string) {
+  const { data, error } = await supabase.from('posts').insert({ content });
+  if (error) throw error;
+  return data;
+}
